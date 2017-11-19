f2bd3129d9bd4d88b14e6368f323839b6bdadc9e
arijeetm <arijeetm@andrew.cmu.edu>
2017-11-18 16:04:44 -0800

    adding scripts for lambda and ec2

---
+++ lambdas/lambda1/hashdeep.py
@@ -0,0 +1,17 @@
+import pymysql
+
+hostname= '54.153.14.97'
+username='ubuntu'
+password='insure'
+database='hashdb'
+
+# Simple routine to run a query on a database and print the results:
+def doQuery( conn ) :
+    cur = conn.cursor()
+    cur.execute( "SELECT fname, lname FROM employee" )
+    for firstname, lastname in cur.fetchall() :
+        print firstname, lastname
+
+myConnection = pymysql.connect( host=hostname, user=username, passwd=password, db=database )
+doQuery( myConnection )
+myConnection.close()
